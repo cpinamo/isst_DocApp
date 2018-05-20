079a38e0c9e20a1f4e07c077b2868233a353ae37
roberguerrero <36502475+roberguerrero@users.noreply.github.com>
2018-05-20 20:09:28 +0200

    Update FormRegistro1PacienteServlet.java

--- src/es/upm/dit/isst/DocApp/servlets/FormRegistro1PacienteServlet.java
+++ src/es/upm/dit/isst/DocApp/servlets/FormRegistro1PacienteServlet.java
@@ -1,17 +1,23 @@
 package es.upm.dit.isst.DocApp.servlets;
 
+
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import es.upm.dit.isst.DocApp.dao.PacienteDAOImplementation;
+import es.upm.dit.isst.DocApp.dao.model.Cita;
 import es.upm.dit.isst.DocApp.dao.model.Paciente;
 
 @WebServlet("/FormRegistro1PacienteServlet")
 public class FormRegistro1PacienteServlet extends HttpServlet {
 
+	private Boolean alerta=false;
+	private Boolean alerta2=false;
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -24,6 +30,33 @@ public class FormRegistro1PacienteServlet extends HttpServlet {
 		String dni = req.getParameter("dni");
 		String domicilio = req.getParameter("domicilio");
 
+		List<Paciente> paciente_list = PacienteDAOImplementation.getInstance().readAllPaciente();
+		
+		for (Paciente pac : paciente_list) {
+			if(pac.getDni()==Integer.parseInt(dni))	{
+				alerta = true;
+			}
+			else {
+				alerta=false;
+			}
+		}
+		for (Paciente pac : paciente_list) {
+			if(pac.getEmail().equals(email))	{
+				alerta2 = true;
+			}
+			else {
+				alerta2=false;
+			}
+		}
+		
+		if (alerta==true || alerta2==true) {
+			req.getSession().setAttribute("alerta", alerta);
+			req.getSession().setAttribute("alerta2", alerta2);	
+			resp.sendRedirect(req.getContextPath() + "/FormRegistroPaciente.jsp");
+		}
+		else {
+		
+		
 		Paciente paciente = new Paciente();
 		paciente.setEmail(email);
 		paciente.setName(name);
@@ -35,7 +68,7 @@ public class FormRegistro1PacienteServlet extends HttpServlet {
 		paciente.setDomicilio(domicilio);
 
 		PacienteDAOImplementation.getInstance().createPaciente(paciente);
-
+		
 		resp.sendRedirect(req.getContextPath() + "/FormLogin.jsp");
 	}
 }
